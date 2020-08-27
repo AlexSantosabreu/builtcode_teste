6c65e3a9c74df55e253d920f407a5ed8960782a6
Alex Abreu <alexsantosdeabreu@gmail.com>
2020-08-27 11:27:59 -0300

    validate crm and  crm_uf

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < ApplicationRecord
 
   validates :name, :crm, presence: true
   validates :crm_uf, length: {is: 2}, presence: true
+  validate :validate_crm_uf, on: :create
 
   def validate_crm_uf
     errors.add(:crm, "this crm exist in that crm_uf") if Doctor.where(crm: self.crm, crm_uf: self.crm_uf).exists?
