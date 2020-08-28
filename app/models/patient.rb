eb176648f08accccba7dced61a65de176d115cf4
Alex Abreu <alexsantosdeabreu@gmail.com>
2020-08-28 11:06:53 -0300

    validates for patients

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,36 @@
 class Patient < ApplicationRecord
+  validates_presence_of :cpf, :name, :birth_date
+  validate :validates_as_cpf
+
+  with_options :allow_blank => true do |v|
+    v.validates_length_of :cpf, :is => 11
+    v.validates_numericality_of :cpf
+    v.validates_uniqueness_of :cpf
+  end
+
+
+  DENY_LIST = %w[
+    00000000000
+    11111111111
+    22222222222
+    33333333333
+    44444444444
+    55555555555
+    66666666666
+    77777777777
+    88888888888
+    99999999999
+    12345678909
+    01234567890
+  ].freeze
+
+  SIZE_VALIDATION = /^[0-9]{11}$/
+
+  def validates_as_cpf
+
+    errors.add(:cpf, "is cpf invalid") if DENY_LIST.include?(self.cpf)
+
+  end
+
+
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Patient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with name, crm and crm_uf' do
+    patient = Patient.create(name: 'Vitoria', birth_date: '2020-08-28', ccpf: 'rj')
+    expect(patient).to be_valid
+  end
+
+  it 'is invalid without name' do
+    patient = Patient.build(name: nil, birth_date: '2020-08-28', cpf: 'rj')
+    patient.valid?
+    expect(patient.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without crm' do
+    patient = Patient.build(name: 'Vitoria', birth_date: 2020-08-28, cpf: 'rj')
+    patient.valid?
+    expect(patient.errors[:crm]).to include("can't be blank")
+  end
+
+  it 'is invalid without crm_uf' do
+    patient = Patient.build(name: 'Vitoria', birth_date: '2020-08-28', cpf: nil)
+    patient.valid?
+    expect(patient.errors[:crm_uf]).to include("can't be blank")
+  end
 end
